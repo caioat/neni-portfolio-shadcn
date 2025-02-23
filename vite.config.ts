7c50ba6e7f289e9f409f964e77998b8c9af6bf95
BuildTools <unconfigured@null.spigotmc.org>
2025-02-23 02:37:00 +0000

    Updating vitse config with base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
+  base: "/",
   plugins: [
     TanStackRouterVite({ autoCodeSplitting: true }),
     react(),
