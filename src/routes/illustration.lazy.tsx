31677aaaaec36d8da5f30244b94e2879242e6fe6
BuildTools <unconfigured@null.spigotmc.org>
2025-03-20 23:27:53 +0000

    Illustration page under construction implementation

--- src/routes/illustration.lazy.tsx
+++ src/routes/illustration.lazy.tsx
@@ -1,9 +1,36 @@
+import { Button } from "@/components/ui/button";
 import { createLazyFileRoute } from "@tanstack/react-router";
+import underConstruction from "@/assets/under_construction.svg";
 
 export const Route = createLazyFileRoute("/illustration")({
   component: Illustration,
 });
 
 function Illustration() {
-  return <>Text</>;
+  return (
+    <div className="flex flex-col items-center justify-center space-y-6">
+      <h2 className="mb-4">Page under construction</h2>
+      <p className="text-xl text-center leading-6.5 mb-4">
+        Sorry, this page is still under development, but it will be ready soon.
+        In the meantime, check out my design work and find out more about me! 😊
+      </p>
+      <div className="mb-8">
+        <Button variant="secondary" className="w-37 text-base" asChild>
+          <a href="/" target="_self">
+            Design cases
+          </a>
+        </Button>
+        <Button
+          variant="ghostSecondary"
+          className="ml-6 w-37 text-base"
+          asChild
+        >
+          <a href="/aboutMe" target="_self">
+            About me
+          </a>
+        </Button>
+      </div>
+      <img src={underConstruction} alt="" />
+    </div>
+  );
 }
